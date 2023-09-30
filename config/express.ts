9b22a47e7a31eedbfaffc4b65da2c0c63fe92895
khaled Jamal <khalidfaqari@gmail.com>
2023-09-30 15:32:30 +0300

    build: added husky

--- config/express.ts
+++ config/express.ts
@@ -1,19 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import fileUpload from 'express-fileupload'
-import bodyParser from 'body-parser'
+import express from 'express';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import bodyParser from 'body-parser';
 const corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  allowedHeaders: 'Content-Type, Authorization, XMLHttpRequest, X-Requested-With, Accept, Origin'
-}
+  allowedHeaders:
+    'Content-Type, Authorization, XMLHttpRequest, X-Requested-With, Accept, Origin',
+};
 
-export const app = express()
-const port = 3000
+export const app = express();
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-app.use(cors(corsOptions))
-app.use(bodyParser({ limit: '50mb' }))
-app.use(fileUpload())
-app.use(express.urlencoded({ extended: true }))
+  console.log(`Example app listening on port ${port}`);
+});
+app.use(cors(corsOptions));
+app.use(bodyParser({ limit: '50mb' }));
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
